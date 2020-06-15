bc5d405604b5b05d6e578e2ca9641d15b76c2835
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-15 11:38:55 +0300

    google oauth login is completed &  urls and ids are defined

--- auth/google.js
+++ auth/google.js
@@ -1,1 +1,16 @@
 const passport = require('passport');
+const GoogleAuth = require('passport-google-oauth20');
+
+const User = require('../models/users');
+
+passport.use(new GoogleAuth({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_LOGIN_SECRET_ID,
+    callbackURL: process.env.GOOGLE_LOGIN_CALLBACK_URL
+}, ((accessToken, refreshToken, profile, done) => {
+    const data = profile._json;
+    console.log(data);
+})));
+
+module.exports = passport;
+
