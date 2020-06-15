3d6adcff91c33ac25ded8695a176510939ea2117
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-15 19:10:20 +0300

    session settings are completed

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET chat page with authentication. */
 router.get('/', function(req, res, next) {
-  res.json('Chat page');
+  res.render('chat', {user: req.user});
 });
 
 module.exports = router;
