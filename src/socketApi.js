fe2abb9dd03ca511be8909e8613da58b8abe39aa
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-16 01:11:51 +0300

    Socket authorization middleware is added to authorize with socket.io

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,12 +1,24 @@
 const socketio = require('socket.io');
 const io = socketio();
+const socketAuthorization = require('../middleware/socketAuthorization');
 
 const socketApi = {
     io
 };
 
+io.use(socketAuthorization); //socket.io authorization
+
+/**
+ * Redis socket.io adapter
+ */
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection', (socket) => {
-    console.log('a user connected to server');
+    console.log('a user connected to server with username', socket.request.user.name);
 });
 
 module.exports = socketApi;
