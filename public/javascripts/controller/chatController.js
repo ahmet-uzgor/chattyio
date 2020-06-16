fa43a888a2bc7827e5a2fd95454b5a123d4bf2df
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-16 12:11:44 +0300

    online userlist method is written

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,9 +1,14 @@
 app.controller('chatController', ['$scope',($scope)=>{
     $scope.activeTab = 2;
+    $scope.onlineList = {};
 
     $scope.changeTab = tab =>{
         $scope.activeTab = tab;
     };
 
     const socket = io.connect("http://me.mydomain.com:3000/");
+    socket.on('onlineList', (users) => {
+        $scope.onlineList = users;
+        $scope.apply();
+    })
 }])
